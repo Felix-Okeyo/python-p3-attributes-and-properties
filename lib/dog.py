4a843d0bcc5f39857d5ca4f2d9a430b8540c3a61
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-21 20:35:25 +0300

    tested and done

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# List of approved dog breeds
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -11,5 +12,40 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+# Dog class
 class Dog:
-    pass
+    def __init__(self):
+        self._name = None
+        self._breed = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+
+
+# # test cases
+# dog1 = Dog()
+# dog1.name = "French Bulldog"
+# print("Dog 1's name:", dog1.name)
+
+dog2 = Dog()
+dog2.breed = "marti"
+print(dog2.breed)
